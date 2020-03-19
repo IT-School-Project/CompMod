4e9dde7fd2ed7fe87f25409b0ed5f5f411575571
FlyingRainbowPotato <60003362+FlyingRainbowPotato@users.noreply.github.com>
2020-03-19 09:46:39 +0100

    Updated so that now all sql works with mariaDB

--- dist/sql/tables.sql
+++ dist/sql/tables.sql
@@ -1,26 +1,30 @@
+CREATE DATABASE compmod;
+
+USE compmod;
+
 CREATE TABLE users (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR NOT NULL,
-    password VARCHAR NOT NULL,
-    first_name VARCHAR NOT NULL,
-    surname VARCHAR NOT NULL,
-    email VARCHAR NOT NULL,
-    address1 VARCHAR NOT NULL,
-    address2 VARCHAR,
+    username VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    first_name VARCHAR(255) NOT NULL,
+    surname VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    address1 VARCHAR(255) NOT NULL,
+    address2 VARCHAR(255),
     post_nr INTEGER NOT NULL,
     img INTEGER NOT NULL
-)
+);
 
 --if profile picture is not provided. automatically make it a basic one 
 
 CREATE TABLE listing (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR NOT NULL,
+    name VARCHAR(255) NOT NULL,
     price FLOAT NOT NULL,
     date DATETIME NOT NULL,
-    desc TEXT NOT NULL,
+    description TEXT NOT NULL,
     user INTEGER NOT NULL
-)
+);
 
 -- IMPORT TABLE post_nr
 
@@ -28,61 +32,61 @@ CREATE TABLE cart (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     listing_id INTEGER NOT NULL,
     user_id INTEGER NOT NULL
-)
+);
 
 CREATE TABLE orders (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     seller INTEGER NOT NULL,
     buyer INTEGER NOT NULL
-)
+);
 
 CREATE TABLE img (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    url VARCHAR NOT NULL
-)
+    url VARCHAR(255) NOT NULL
+);
 
 CREATE TABLE ximg (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     listing_id INTEGER NOT NULL,
     img_id INTEGER NOT NULL
-)
+);
 
 CREATE TABLE xparts (
-    id INTEGER AUTO_INCREMENT PRIMARY KEY
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
     listing_id INTEGER NOT NULL,
     parts_id INTEGER NOT NULL,
     important BINARY NOT NULL
-)
+);
 
 CREATE TABLE parts (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR NOT NULL
-)
+    name VARCHAR(255) NOT NULL
+);
 
 CREATE TABLE xcategory (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     category_id INTEGER NOT NULL,
     parts_id INTEGER NOT NULL
-)
+);
 
 CREATE TABLE category (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    category VARCHAR NOT NULL
-)
+    category VARCHAR(255) NOT NULL
+);
 
 CREATE TABLE xspec (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     spec_id INTEGER NOT NULL,
     parts_id INTEGER NOT NULL
-)
+);
 
 CREATE TABLE spec (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    spec VARCHAR NOT NULL,
+    spec VARCHAR(255) NOT NULL,
     type INTEGER NOT NULL
-)
+);
 
 CREATE TABLE type (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR NOT NULL
-)
+    name VARCHAR(255) NOT NULL
+);
