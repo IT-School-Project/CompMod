fad77342373b948d6a24eab90b5bb6f35424f773
Leif Eggenfellner <leif.eggenfellner@gmail.com>
2020-03-19 13:01:47 +0100

    added favcon to website

--- dist/php/store.php
+++ dist/php/store.php
@@ -7,6 +7,7 @@
     <link rel="stylesheet" type="text/css" href="/dist/css/main.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
     integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+    <link rel="shortcut icon" type="image/png" href="../img/favcon.png">
 </head>
 <body>
     <?php require '../inc/nav.php'?>
