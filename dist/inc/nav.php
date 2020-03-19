542c568933bc4e83adf90324caa2bac21a06477b
Leif Eggenfellner <leif.eggenfellner@gmail.com>
2020-03-19 10:09:33 +0100

    changed file setup

---
+++ dist/inc/nav.php
@@ -0,0 +1,20 @@
+<header>
+<nav class="topnav fixed-top">
+        <ul class="navbar-nav">
+            <img src="../img/logo.png" alt="">
+            <li class="nav-item"><a class="nav-link" href="">Home</a></li>
+            <li class="nav-item"><a class="nav-link" href="">Shop</a></li>
+        </ul>
+        <form action="" method="get" class="form-inline">
+            <input type="text" class="form-control" placeholder="Search">
+            <i></i>
+        </form>
+        <ul class="logos">
+            <li class="logo headset"><a href="#"><i class="fas fa-headset"></i>Customer Service</a></li>
+            <li class="logo heart"><a href="#"><i class="far fa-heart"></i>Wishlist</a></li>
+            <li class="logo user"><a href="#"><i class="far fa-user"></i>User</a></li>
+            <li class="logo cart"><a href="#"><i class="fas fa-shopping-cart"></i>0,00</a></li>
+        </ul>
+    </nav>
+</header>
+    
