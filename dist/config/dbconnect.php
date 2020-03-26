57c368b1d11ce1909b5e0881cdbc8fd357044a83
FlyingRainbowPotato <patrickfalkeid@gmail.com>
2020-03-26 10:01:16 +0100

    variablenames not conflicting any longer

--- dist/config/dbconnect.php
+++ dist/config/dbconnect.php
@@ -1,13 +1,13 @@
 <?php 
   //settings
   $servername = "localhost:3307";
-  $username = "root";
-  $password = "";
+  $dbusername = "root";
+  $dbpassword = "";
   $dbname = "compmod";
 
   //open connection
   $conn = mysqli_connect(
-    $servername, $username, $password, $dbname
+    $servername, $dbusername, $dbpassword, $dbname
   );
   
   mysqli_set_charset($conn, "utf-8");
