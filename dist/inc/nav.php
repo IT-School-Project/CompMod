1e97ced627545287e069827ffcea4aa6d473a0ad
Leif Eggenfellner <leif.eggenfellner@gmail.com>
2020-03-27 14:27:58 +0100

    trashed it

--- dist/inc/nav.php
+++ dist/inc/nav.php
@@ -1,19 +1,19 @@
-<nav class="navbar">
-    <section class="menu">
-        <ul>
-            <li><a href="#"><img class="img" src="../img/logo.png" alt=""></a></li>
-            <li><a class="menu-item" href="#">Home</a></li>
-            <li><a class="menu-item" href="#">Shop</a></li>
-            <li><a class="menu-item" href="#">Wishlist</a></li>
-            <li><a class="menu-item" href="#">Customer Service</a></li>
-            <li><a id="search" href="#"><i class="fa fa-search"></i></a></li>
-            <li><a href="#"><i class="fa fa-shopping-basket"></i></a></li>
-            <li><?php include "user_handler.php" ?></li>
-        </ul>
-        <section class="search-form">
-            <form>
-                <input type="text" name="" placeholder="Search">
-            </form>
+<nav class="navbar navbar-inverse">
+    <section class="container-fluid">
+        <section class="navbar-header">
+            <a href="" class="navbar-brand"><img src="../img/logo.png" alt=""></a>
         </section>
+        <ul class="navbar navbar-nav">
+            <li><a href="">Home</a></li>
+            <li><a href="../php/store.php">Shop</a></li>
+            <li><a href="">Wishlist</a></li>
+            <li><a href="">Customer Service</a></li>
+            
+        </ul>
+        <ul class="nav navbar-nav navbar-right">
+            <li><a id="search" href=""><i class="fa fa-search"></i></a></li>
+            <li><a href=""><i class="fa fa-shopping-basket"></i></
+            <a class="button">Login</a>
+        </ul>
     </section>
 </nav>
