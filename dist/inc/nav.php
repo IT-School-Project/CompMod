ad4e15fbf76e0aaff7dd5ba18e964b95b23890a8
Leif Eggenfellner <leif.eggenfellner@gmail.com>
2020-03-31 09:54:59 +0200

    added link to log in

--- dist/inc/nav.php
+++ dist/inc/nav.php
@@ -1,38 +1,36 @@
-<<<<<<< HEAD
-<nav class="navbar navbar-inverse">
-    <section class="container-fluid">
-        <section class="navbar-header">
-            <a href="" class="navbar-brand"><img src="../img/logo.png" alt=""></a>
-=======
 <nav class="navbar">
-    <section class="menu">
+    <section class="dropdown">
+        <button class="dd-menu"><i class="fa fa-bars"></i></button>
         <ul>
-            <li><a href="#"><img class="img" src="../img/logo.png" alt=""></a></li>
-            <li><a class="menu-item" href="#">Home</a></li>
-            <li><a class="menu-item" href="/compmod/dist/php/store.php">Shop</a></li>
-            <li><a class="menu-item" href="#">Wishlist</a></li>
-            <li><a class="menu-item" href="#">Customer Service</a></li>
-            <li><a id="search" href="#"><i class="fa fa-search"></i></a></li>
-            <li><a href="#"><i class="fa fa-shopping-basket"></i></a></li>
-            <li><?php include "user_handler.php" ?></li>
+        <li><a href="">Home</a></li>
+        <li><a href="../php/store.php">Store</a></li>
+        <li><a href="">Wishlist</a></li>
+        <li><a href="">Contact</a></li>
         </ul>
-        <section class="search-form">
-            <form>
-                <input type="text" name="" placeholder="Search">
-            </form>
->>>>>>> d15f8e7a06a7cfeaf0e6ac9031b5372bd314d79c
-        </section>
-        <ul class="navbar navbar-nav">
-            <li><a href="">Home</a></li>
-            <li><a href="../php/store.php">Shop</a></li>
-            <li><a href="">Wishlist</a></li>
-            <li><a href="">Customer Service</a></li>
-            
-        </ul>
-        <ul class="nav navbar-nav navbar-right">
+    </section>
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        <ul class="navbar-right">
             <li><a id="search" href=""><i class="fa fa-search"></i></a></li>
+            <li><a class="button" href="../php/user_handler.php">Login</a></li>
+            <li><a class="button" href="../php/sign_up.php">Signup</a></li>
             <li><a href=""><i class="fa fa-shopping-basket"></i></
-            <a class="button">Login</a>
         </ul>
     </section>
 </nav>
