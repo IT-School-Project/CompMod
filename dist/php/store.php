bef114ac0cfe29eef3ccae9a73a3fcf9e3add7fa
FlyingRainbowPotato <patrickfalkeid@gmail.com>
2020-04-03 12:05:03 +0200

    trying to do some dynamic

--- dist/php/store.php
+++ dist/php/store.php
@@ -2,7 +2,10 @@
 <?php require '../inc/header.php'?>
 <body>
     <?php require '../inc/nav.php'?>
-
+    <form action = "newitem.php" method = "POST">
+        <input type = "hidden" name = "new" value = "true">
+        <input type = "submit" value = "sell an item">
+    </form>
     <?php require '../inc/script.php'?>
 </body>
 </html>
