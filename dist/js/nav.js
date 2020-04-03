794bc265e0558e8610d495f68a8e748e39756fc5
Leif Eggenfellner <leif.eggenfellner@gmail.com>
2020-04-03 13:55:32 +0200

    made able to close and open login form

--- dist/js/nav.js
+++ dist/js/nav.js
@@ -1,8 +1,16 @@
-$(document).ready(function () {
-    $('a.dd-menu').click(function () {
-        $('ul.dropdown_list').toggleClass('active')
-    })
-    $('.dropdown_list').on('mouseleave', function () {
-        $(this).removeClass('active')
-    });
+$(document).ready(function() {
+  $("a.dd-menu").click(function() {
+    $("ul.dropdown_list").toggleClass("active");
+  });
+  $(".dropdown_list").on("mouseleave", function() {
+    $(this).removeClass("active");
+  });
+});
+
+document.getElementById("button").addEventListener("click", function() {
+  document.querySelector(".popup").style.display = "flex";
+});
+
+document.querySelector(".close").addEventListener("click", function() {
+  document.querySelector(".popup").style.display = "none";
 });
