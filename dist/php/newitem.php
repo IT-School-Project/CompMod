649ee4e22e14ecf6b0a1e5cce1f88d697f5b4373
FlyingRainbowPotato <patrickfalkeid@gmail.com>
2020-04-14 12:28:25 +0200

    æææææææ

--- dist/php/newitem.php
+++ dist/php/newitem.php
@@ -1,6 +1,6 @@
 <?php 
-session_start();
 require '../config/dbconnect.php';
+require '../inc/header.php';
 
 if(isset($_SESSION['userid']) && isset($_POST['title']) && isset($_POST['price']) && isset($_POST['description']) && is_numeric($_POST['price'])){
   $userid = $_SESSION['userid'];
@@ -14,14 +14,17 @@ if(isset($_SESSION['userid']) && isset($_POST['title']) && isset($_POST['price']
   $result = mysqli_query($conn, $sql);
 }
 
-else{
-  echo "you entered wrong values.";
+
+
+if(isset($_POST['category']) && isset($_POST['partName'])){
+  $category = $_POST['category'];
+  $partName = $_POST['partName'];
 }
-require '../inc/header.php';
 ?>
 <body>
   <?php require '../inc/nav.php'?>
-  <form method = "POST">
+  <form id = "listing" method = "POST">
+
     <label for="inputTitle">Title</label>
     <input 
       id="inputTitle"
@@ -29,6 +32,7 @@ require '../inc/header.php';
       type="text"
       placeholder="Grab some attention"
     >
+
     <label for="inputPrice">Price</label>
     <input 
       id="inputPrice"
@@ -36,6 +40,7 @@ require '../inc/header.php';
       type="text"
       placeholder="A fair price"
     >
+
     <label for="inputDescription">Description</label>
     <textarea rows="4" cols="50" 
       id="inputDescription"
@@ -43,8 +48,68 @@ require '../inc/header.php';
       type="text"
       placeholder="Make them trust you"
     ></textarea>
-    <input type = "submit">
+
+    <br><br>
+    
+    
+    <?php
+    $categories = array();
+    $sql2 = "SELECT id,category FROM category";
+    $result2 = mysqli_query($conn, $sql2);
+    while($row = mysqli_fetch_array($result2))
+    {
+      array_push(
+        $categories, $row['category']
+      );
+    }
+    $js_array = json_encode($categories);
+    ?>
+    <script type = 'text/javascript'>
+      var choices = <?php echo $js_array; ?>;
+      var addInputNum = 0;
+      function addInput(selectName) {
+        var newLabel = document.createElement('label');
+        newLabel.setAttribute("for", "inputPartName")
+        newLabel.innerHTML = "Name";
+        document.getElementById(selectName).appendChild(newLabel);
+
+        var newText = document.createElement('input');
+        var selectHTML = "";
+        newText.setAttribute("id", "inputPartName")
+        newText.setAttribute("name", "partName[]")
+        newText.setAttribute("type", "text")
+        newText.setAttribute("placeholder", "example: Intel i7 9700k")
+        newText.innerHTML = selectHTML;
+        document.getElementById(selectName).appendChild(newText);
+
+
+
+        var newLabel = document.createElement('label');
+        newLabel.setAttribute("for", "selectCategory")
+        newLabel.innerHTML = "Select Part Category";
+        document.getElementById(selectName).appendChild(newLabel);
+
+        var newSelect = document.createElement('select');
+        var selectHTML = "";
+        for(i = 0; i < choices.length; i = i + 1) {
+          selectHTML += "<option value='" + choices[i] + "'>" + choices[i] + "</option>";
+        }
+        newSelect.setAttribute("id", "selectCategory")
+        newSelect.setAttribute("name", "category[]")
+        newSelect.innerHTML = selectHTML;
+        document.getElementById(selectName).appendChild(newSelect);
+
+        addInputNum++
+      }
+  </script> 
+  <input type="button" value="Add" onclick="addInput('listing');" />
+  <input type = "submit">
+
+
   </form>
+  
+
+</form>
   <?php require '../inc/script.php'?>
 </body>
 </html>
