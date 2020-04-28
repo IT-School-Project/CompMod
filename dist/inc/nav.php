fe48551bf6c8a5db1aef7c1c60320012df596c38
Leif Eggenfellner <leif.eggenfellner@gmail.com>
2020-04-28 14:22:31 +0200

    Changed layout

--- dist/inc/nav.php
+++ dist/inc/nav.php
@@ -1,52 +1,50 @@
 <nav class="navbar">
-    <section class="dropdown">
-        <a class="dd-menu"><i class="fa fa-bars"></i></a>
-        <ul class="dropdown_list">
-            <li class="list-item"><a href="">Home</a></li>
-            <li class="list-item"><a href="../php/store.php">Store</a></li>
-            <li class="list-item"><a href="">Wishlist</a></li>
-            <li class="list-item"><a href="">Contact</a></li>
-        </ul>
-        <img class="logo" src="../img/logo.png" alt="">
-    </section>
+    <ul class="nav">
+        <li class="dropdown-li">
+            <section class="dropdown">
+                <a class="dd-menu"><i class="fa fa-bars"></i></a>
+                <ul class="dropdown_list">
+                    <li class="list-item"><a href="">Home</a></li>
+                    <li class="list-item"><a href="../php/store.php">Store</a></li>
+                    <li class="list-item"><a href="">Wishlist</a></li>
+                    <li class="list-item"><a href="">Contact</a></li>
+                </ul>
+            </section>
+        </li>
+
+        <li class="img">
+            <img class="logo" src="../img/logo.png" alt="">
+        </li>
+
+        <li class="search-bar">
+            <section class="search">
+                <form action="" method="get" class="search-form">
+                    <input type="text" name="search" class="search-bar" placeholder="Search">
+                    <a onclick="this.closest('form').submit();return false;"><i class="icon fa fa-search"></i></a>
+                </form>
+            </section>
+        </li>
+
+        <li class="login">
+            
+        </li>
+        <li class="signup">
+            <a class="button" href="../php/sign_up.php">Sign up</a>
+        </li>
+
+        <li class="cart">
+            <a href=""><i class="fa fa-shopping-basket"></i></a>
+        </li>
+    </ul>
+        
     
-    <section class="search">
-        <form action="" method="get" class="search-form">
-            <input type="text" name="search" class="search-bar" placeholder="Search">
-            <a onclick="this.closest('form').submit();return false;"><i class="icon fa fa-search"></i></a>
-        </form>
-    </section>
     <section class="nav-outer">
         <section class="table">
             <ul class="navbar-right">
-                <li class="login"><a id="button" href="#">Login</a></li>
-                <li class="signup"><a class="button" href="../php/sign_up.php">Sign up</a></li>
-                <li class="cart"><a href=""><i class="fa fa-shopping-basket"></i></a></li>
+                <li class="cart"></li>
             </ul>
         </section>
     </section>
 
     <?php require '../config/dbconnect.php';?>
-    
-    <section class="popup">
-        <section class="wrapper">
-            <h2>Login</h2>
-            <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
-                <section class="form-group">>
-                    <label>Username</label>
-                    <input type="text" name="username" class="form-control">
-                    <span class="help-block"></span>
-                </section>
-                <section class="form-group">
-                    <label>Password</label>
-                    <input type="password" name="password" class="form-control">
-                    <span class="help-block"></span>
-                </section>
-                <section class="form-group">
-                    <input type="submit" class="btn btn-primary" value="Login">
-                </section>
-                <p>Don't have an account? <a href="../php/sign_up.php">Sign up</a>.</p>
-            </form>
-        </section>
-    </section>
 </nav>
