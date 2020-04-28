ed671b00821eb5d5486d341ea91152e286814a62
Leif Eggenfellner <leif.eggenfellner@gmail.com>
2020-04-28 14:43:55 +0200

    changed css file link path

--- dist/inc/header.php
+++ dist/inc/header.php
@@ -4,9 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CompMod</title>
-    <link rel="stylesheet" type="text/css" href="../css/nav.css">
-    <link rel="stylesheet" type="text/css" href="../css/admin.css">
-    <link rel="stylesheet" href="../css/sign_up.css">
+    <link rel="stylesheet" type="text/css" href="../../sass/nav.css">
+    <link rel="stylesheet" type="text/css" href="../../sass/admin.css">
+    <link rel="stylesheet" href="../../sass/sign_up.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
     integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
