cabd18c112f1b2ccc5aa03dcc09c025daf0f7ee4
FlyingRainbowPotato <patrickfalkeid@gmail.com>
2020-04-30 21:43:35 +0200

    picture support

--- dist/php/store.php
+++ dist/php/store.php
@@ -1,7 +1,7 @@
 <?php require '../config/dbconnect.php';
 require '../inc/header.php';
 
-$sql = "SELECT l.id, l.name, l.price, u.post_nr, u.address1
+$sql = "SELECT l.id, l.name, l.price, l.img, u.post_nr, u.address1
 FROM listing l, users u
 WHERE l.user = u.id
 ORDER BY l.date";
@@ -19,7 +19,7 @@ $result = mysqli_query($conn,$sql);
         <a href = 'listing.php?id=".$row['id']."'>
         <section class = 'listing'>
             <h3>".$row['name']."</h3>
-            <img src='../img/placeholder.png' alt='placeholder image' height = '200' width = '200'>
+            <img src='".$row['img']."' alt='placeholder image' height = '200' width = '200'>
             <p class = 'price'>" .$row['price']."</p>
             <p class = 'address'>".$row['post_nr'].", ".$row['address1']."</p>
         </section>
